d9b7a1baa485b88e917085ba9b17860b43982c4b
Marcus <mpt789@gmail.com>
2020-07-13 18:33:21 -0700

    set up init routes inside app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar"
 // Styles
 import "./App.css";
 
@@ -10,7 +10,12 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/">
+        <Navbar />
+        <Home />
+      </Route>
+      <Route exact path="/about"></Route>
+      <Route exact path="/contact"></Route>
     </Router>
   );
 }
