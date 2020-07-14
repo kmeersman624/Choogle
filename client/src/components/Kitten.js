ced88c74abb9c2cbcb45c29588bae1bef7db5551
daniel molina <bigdpower@icloud.com>
2020-07-13 17:30:40 -0700

    added materialize

--- client/src/components/Kitten.js
+++ client/src/components/Kitten.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import "materialize-css";
+import Button from "react-materialize";
 
 function Kitten({ sleepy, name, onClick }) {
   return (
     <div>
       <h2>{name}</h2>
-      <Button variant={sleepy ? "primary" : "warning"} onClick={onClick}>
-        {sleepy ? "Wake Up" : "Put to Bed"}
+      <Button  onClick={onClick}>
+        
       </Button>
     </div>
   );
