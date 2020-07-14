afd627a710fc0cb1b726830e91a2ac72b06f7fb1
Marcus <mpt789@gmail.com>
2020-07-14 00:34:17 -0700

    added round buttons inside of header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import {Navbar, NavItem, Icon} from 'react-materialize'
+import { Navbar, NavItem, Icon, Button } from 'react-materialize'
 import logo from '../../images/chooglelogo.png'
+import '../Header/style.css'
 
 export default function Header() {
     return (
         <>
             <Navbar
+                className="white"
                 alignLinks="right"
-                brand={<a className="brand-logo" href="#"><img src={logo} height="50px" width="auto"/></a>}
+                brand={<a className="brand-logo logoMargin" href="#"><img src={logo} height="60px" width="auto" alt="" /></a>}
                 id="mobile-nav"
                 menuIcon={<Icon>menu</Icon>}
                 options={{
@@ -23,11 +25,46 @@ export default function Header() {
                 }}
             >
                 <NavItem href="">
-                    Getting started
-  </NavItem>
-                <NavItem href="components.html">
-                    Components
-  </NavItem>
+                    <Button
+                        className="btn-large yellow"
+                        floating
+                        icon={<Icon className="fontSize  black-text">Home</Icon>}
+                        large
+                        node="button"
+                    />
+                </NavItem>
+                <NavItem href="">
+                    <Button
+                        className="btn-large red"
+                        floating
+                        icon={<Icon className="fontSize">About</Icon>}
+                        node="button"
+                    />
+                </NavItem>
+                <NavItem href="">
+                    <Button
+                        className="btn-large green"
+                        floating
+                        icon={<Icon className="fontSize">Notes</Icon>}
+                        node="button"
+                    />
+                </NavItem>
+                <NavItem href="">
+                    <Button
+                        className="btn-large blue"
+                        floating
+                        icon={<Icon className="fontSizeContact">Contact</Icon>}
+                        node="button"
+                    />
+                </NavItem>
+                <NavItem href="">
+                    <Button
+                        className="btn-large yellow"
+                        floating
+                        icon={<Icon className="fontSize black-text">Login</Icon>}
+                        node="button"
+                    />
+                </NavItem>
             </Navbar>
         </>
     )
