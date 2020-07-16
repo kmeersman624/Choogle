61bb9dd150504cce39c8aa2e8bcc522d63f263f1
daniel molina <bigdpower@icloud.com>
2020-07-15 18:52:28 -0700

    sign in and log in page are up

--- client/src/pages/LogIn/index.js
+++ client/src/pages/LogIn/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Container, Col, Row, TextInput, Button } from "react-materialize";
+import login from "../../images/LoginLink.png"
+import "../LogIn/styles.css"
 
 function LogIn() {
-    return (
-        <div>
-            <h1>log in</h1>
-        </div>
-    )
+  return (
+    
+      <Container>
+        <Row>
+          <Col className=" center s3">
+            <img className="signin" src={login}/>
+            <TextInput id="TextInput-6" label="Email" />
+            <TextInput id="TextInput-4" label="Password" />
+            <Button className="submit" node="button" type="submit" waves="light">
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    
+  );
 }
 
-export default LogIn
+export default LogIn;
