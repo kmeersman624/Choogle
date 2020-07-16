383fd8d85ce450927122efe9162b5cf210a4067c
daniel molina <bigdpower@icloud.com>
2020-07-15 17:43:15 -0700

    signup and login pages added and routed

---
+++ client/src/pages/LogIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogIn() {
+    return (
+        <div>
+            <h1>log in</h1>
+        </div>
+    )
+}
+
+export default LogIn
---
+++ client/src/pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+    return (
+        <div>
+            <h1>sign up page!</h1>
+        </div>
+    )
+}
+
+export default SignUp
