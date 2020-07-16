ea9ca2a18cf9757a12886675d36934b2cc649f83
Marcus <mpt789@gmail.com>
2020-07-15 22:30:31 -0700

    added links and styling to login and signup pages

--- client/src/pages/LogIn/index.js
+++ client/src/pages/LogIn/index.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { Container, Col, Row, Button } from "react-materialize";
+import { Link } from 'react-router-dom'
+import { Container, Col, Row, Button, } from "react-materialize";
 import login from "../../images/LoginLink.png"
 import "../LogIn/styles.css"
 
 function LogIn() {
   return (
     
-      <Container>
+      <Container className="container-fluid">
         <Row className="verticalAlignItems">
           <Col className="s4"/>
-          <Col className="center s4">
+          <Col className="center s4 margin-top">
             <img className="signin" src={login}/>
             <input size="60" id="TextInput-6" placeholder="Email" />
             <input size="60" id="TextInput-4" placeholder="Password" />
-            <Button className="submit" node="button" type="submit" waves="light">
+            <Button className="submit large" node="button" type="submit" waves="light">
               Submit
             </Button>
+            <Row>
+              <Col className="s12 margin-top">
+                <h5>Login in or Sign up <Link to="/signup">here!</Link></h5>
+              </Col>
+            </Row>
           </Col>
           <Col className="s4"/>
         </Row>
--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { Container, Col, Row, TextInput, Button } from "react-materialize";
+import { Link } from 'react-router-dom'
+import { Container, Col, Row, Button } from "react-materialize";
 import signin from "../../images/SignInLink.png"
 import "../SignUp/styles.css"
 
 function SignUp() {
   return (
     
-      <Container>
+      <Container className="container-fluid">
         <Row>
-          <Col className=" center s3">
+        <Col className="s4"/>
+          <Col className=" center s4 margin-top">
             <img className="signin" src={signin}/>
-            <TextInput id="TextInput-6" label="Email" />
-            <TextInput id="TextInput-4" label="Password" />
+            <input size="60" id="TextInput-6" placeholder="Email" />
+            <input size="60" id="TextInput-4" placeholder="Password" />
             <Button className="submit" node="button" type="submit" waves="light">
               Submit
             </Button>
+            <Row>
+              <Col className="s12 margin-top">
+                <h5>Sign up or Login in <Link to="/login">here!</Link></h5>
+              </Col>
+            </Row>
           </Col>
+          <Col className="s4"/>
         </Row>
       </Container>
     
