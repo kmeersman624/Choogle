1ac96432dff93a0a0e8d15ac6046b61ecfb68b09
daniel molina <bigdpower@icloud.com>
2020-07-16 00:24:21 -0700

    signup

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Col, Row, TextInput, Button } from "react-materialize";
 import signin from "../../images/SignInLink.png"
+import axios from "axios";
 import "../SignUp/styles.css"
 
-function SignUp() {
-  return (
-    
+
+
+class SignUp extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      userId: "",
+      Email: "",
+      Password: ""
+    }
+  }
+
+  changehandler = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    console.log(this.state)
+    axios
+    .post('/signup', this.state)
+    TouchEvent(response => {
+      console.log(response)
+    })
+  }
+
+  render(){
+   const { Email, Password } = this.state
+    return(
       <Container>
         <Row>
           <Col className=" center s3">
@@ -18,8 +46,8 @@ function SignUp() {
           </Col>
         </Row>
       </Container>
-    
-  );
+    )
+  }
 }
 
 export default SignUp;
