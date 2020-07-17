1df56b97f9f230c48ec3a81b1490f16c3801732b
Marcus <mpt789@gmail.com>
2020-07-16 23:56:43 -0700

    cleaned home page and added tilt

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 import logo from "../../images/chooglelogo.png";
 import 'materialize-css';
 import { Button, Card, Row, Col } from 'react-materialize';
@@ -6,8 +7,8 @@ import { Button, Card, Row, Col } from 'react-materialize';
 function Home() {
   return (
     <div>
-      <div className="center col s12">
-        <img className="mainlogo" src={logo} alt={logo}/>
+      <div className="container center">
+        <Tilt><img className="mainlogo" src={logo} alt={logo}/></Tilt>
       </div> 
     </div>
   );
