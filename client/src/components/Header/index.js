f95ae8ebb0492e84aa597ac2198ad12f76a9e0bd
daniel molina <bigdpower@icloud.com>
2020-07-18 19:13:11 -0700

    seperated sidenav from header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,52 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import {
-  Navbar,
-  NavItem,
-  Icon,
-  Button,
-  SideNav,
-  SideNavItem,
-  TextInput,
-  Card,
-} from "react-materialize";
+import { Navbar, Icon } from "react-materialize";
 import API from "../../utils/API";
-
 import logo from "../../images/Choogle.png";
-import circlelogo from "../../images/chooglelogo.png";
-import welcome from "../../images/welcome.png";
-import save from "../../images/save.png";
-import del from "../../images/delete.png";
-import update from "../../images/update.png";
-import signup from "../../images/signin@2x.png";
-import HomeLink from "../../images/home@2x.png";
-import AboutLink from "../../images/about@2x.png";
-import NotesLink from "../../images/notes@2x.png";
-import ContactLink from "../../images/contact@2x.png";
 import "./style.css";
 
 class Header extends Component {
-  state = {
-    notes: [],
-  };
-
-  componentDidMount() {
-    this.loadNotes();
-  }
-
-  loadNotes = () => {
-    API.getNotes()
-      .then((res) => this.setState({ notes: res.data }))
-      .catch((err) => console.log(err));
-  };
-
-  // Deletes a note from the database with a given id, then reloads notes from the db
-  deleteNote = (id) => {
-    API.deleteNote(id)
-      .then((res) => this.loadNotes())
-      .catch((err) => console.log(err));
-  };
-
   render() {
     return (
       <>
@@ -97,96 +56,6 @@ class Header extends Component {
                             node="button"
                         /> */}
           </Link>
-
-          <SideNav
-            className="sideNavWidth waves-effect waves-grey btn-flat"
-            id="SideNav-10"
-            options={{
-              draggable: true,
-              edge: "right",
-              scrollable: true,
-            }}
-            trigger={
-              <a href="#" className="waves-effect waves-grey btn-flat">
-                <font className="notesTEXT" styles="vertical-align: inherit;">
-                  Notes
-                </font>
-              </a>
-            }
-          >
-            <SideNavItem className="col s12 center-align">
-              <img src={welcome} alt={welcome} height="50px" />
-            </SideNavItem>
-            <div className="row">
-              <SideNavItem className="col s12">
-                <div className="row">
-                  <div className="col s6 left-align">
-                    <img src={circlelogo} alt={circlelogo} height="200px" />
-                  </div>
-                  <div className="col s6">
-                    <div className="row">
-                      <div className="col s12 marginTop">
-                        Username: Choogle Foo
-                      </div>
-                      <div className="col s12">Email: chooglefoo@gmail.com</div>
-                    </div>
-                  </div>
-                </div>
-              </SideNavItem>
-            </div>
-            <div className="row">
-              <SideNavItem>
-                <h2>Notes:</h2>
-                {this.state.notes.map((note) => (
-                  <>
-                    <Card
-                      actions={[
-                        <Button
-                          node="a"
-                          small
-                          style={{
-                            marginRight: "5px",
-                          }}
-                          waves="light"
-                        >
-                          Save
-                        </Button>,
-                        <Button
-                          node="a"
-                          small
-                          style={{
-                            marginRight: "5px",
-                          }}
-                          waves="light"
-                        >
-                          Update
-                        </Button>,
-                        <Button
-                          node="a"
-                          small
-                          style={{
-                            marginRight: "5px",
-                          }}
-                          waves="light"
-                        >
-                          Delete
-                        </Button>,
-                      ]}
-                      className="blue-grey darken-1"
-                      closeIcon={<Icon>close</Icon>}
-                      revealIcon={<Icon>more_vert</Icon>}
-                      textClassName="white-text"
-                      header={note.topic}
-                      title={note.url}
-                    >
-                      {note.notes}
-                    </Card>
-                  </>
-                ))}
-              </SideNavItem>
-            </div>
-            <SideNavItem divider />
-          </SideNav>
 
           <Link className="waves-effect waves-grey btn-flat" to="/contact">
             <font styles="vertical-align: inherit;">Contact</font>
--- client/src/components/SideComponent/index.js
+++ client/src/components/SideComponent/index.js
@@ -1,51 +1,123 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Icon, Button, SideNav, SideNavItem, Card } from "react-materialize";
+import API from "../../utils/API";
+import welcome from "../../images/welcome.png";
+import circlelogo from "../../images/chooglelogo.png";
 
-import { Icon, Button, SideNav, SideNavItem } from 'react-materialize'
+class SideComponent extends Component {
+    state = {
+        notes: [],
+    };
 
-export default function SideComponent() {
-    return (
-        <>
+    componentDidMount() {
+        this.loadNotes();
+    }
+
+    loadNotes = () => {
+        API.getNotes()
+            .then((res) => this.setState({ notes: res.data }))
+            .catch((err) => console.log(err));
+    };
+
+    // Deletes a note from the database with a given id, then reloads notes from the db
+    deleteNote = (id) => {
+        API.deleteNote(id)
+            .then((res) => this.loadNotes())
+            .catch((err) => console.log(err));
+    };
+
+    render() {
+        return (
             <SideNav
+                className="sideNavWidth waves-effect waves-grey btn-flat"
                 id="SideNav-10"
                 options={{
                     draggable: true,
-                    edge: "right"
+                    edge: "right",
+                    scrollable: true,
                 }}
                 trigger={
-                    <Button
-                        className="btn-large green notesBtn"
-                        floating
-                        icon={<Icon className="fontSize">Notes</Icon>}
-                        node="button"
-                    />
+                    <a href="#" className="waves-effect waves-grey btn-flat">
+                        <font className="notesTEXT" styles="vertical-align: inherit;">
+                            Notes
+            </font>
+                    </a>
                 }
             >
-                <SideNavItem
-                    user={{
-                        background: '',
-
-                        name: 'John Doe',
-                        email: 'jdandturk@gmail.com'
-                    }}
-                    userView
-                />
-                <SideNavItem
-
-                >
-                    Saved Notes
-                        </SideNavItem>
-
-                <SideNavItem divider />
-                <SideNavItem subheader>
-                    Resources
-                        </SideNavItem>
-                <SideNavItem
-                    href="#!third"
-                    waves
-                >
-                    Saved Links
+                <SideNavItem className="col s12 center-align">
+                    <img src={welcome} alt={welcome} height="50px" />
+                </SideNavItem>
+                <div className="row">
+                    <SideNavItem className="col s12">
+                        <div className="row">
+                            <div className="col s6 left-align">
+                                <img src={circlelogo} alt={circlelogo} height="200px" />
+                            </div>
+                            <div className="col s6">
+                                <div className="row">
+                                    <div className="col s12 marginTop">Username: Choogle Foo</div>
+                                    <div className="col s12">Email: chooglefoo@gmail.com</div>
+                                </div>
+                            </div>
+                        </div>
                     </SideNavItem>
+                </div>
+                <div className="row">
+                    <SideNavItem>
+                        <h2>Notes:</h2>
+                        {this.state.notes.map((note) => (
+                            <>
+                                <Card
+                                    actions={[
+                                        <Button
+                                            node="a"
+                                            small
+                                            style={{
+                                                marginRight: "5px",
+                                            }}
+                                            waves="light"
+                                        >
+                                            Save
+                    </Button>,
+                                        <Button
+                                            node="a"
+                                            small
+                                            style={{
+                                                marginRight: "5px",
+                                            }}
+                                            waves="light"
+                                        >
+                                            Update
+                    </Button>,
+                                        <Button
+                                            node="a"
+                                            onClick={this.deleteNote}
+                                            small
+                                            style={{
+                                                marginRight: "5px",
+                                            }}
+                                            waves="light"
+                                        >
+                                            Delete
+                    </Button>,
+                                    ]}
+                                    className="blue-grey darken-1"
+                                    closeIcon={<Icon>close</Icon>}
+                                    revealIcon={<Icon>more_vert</Icon>}
+                                    textClassName="white-text"
+                                    header={note.topic}
+                                    title={note.url}
+                                >
+                                    {note.notes}
+                                </Card>
+                            </>
+                        ))}
+                    </SideNavItem>
+                </div>
+                <SideNavItem divider />
             </SideNav>
-        </>
-    )
+        );
+    }
 }
+
+export default SideComponent;
