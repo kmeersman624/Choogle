e0f089bcaf5c4b59a7d9a7ac72528c46b9351f48
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-07-20 08:21:46 -0700

    ChoogleCard and Json files

---
+++ client/src/components/ChoogleCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+function ChoogleCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.image} alt={props.content}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.content}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button href={props.link} size="small" color="primary">
+          CHOOGLE IT
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+export default ChoogleCard;
