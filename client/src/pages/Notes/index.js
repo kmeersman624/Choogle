05a7a13b6a344c4f4933f57101e4606c4bfe026e
Marcus <mpt789@gmail.com>
2020-07-20 14:14:45 -0700

    update

--- client/src/pages/Notes/index.js
+++ client/src/pages/Notes/index.js
@@ -4,47 +4,9 @@ import NoteCard from "../../components/NoteCard";
 import API from "../../utils/API";
 
 class Notes extends Component {
-<<<<<<< HEAD
   state = {
     notes: [],
-    email: [],
   };
-=======
-    state = {
-        notes: [],
-      };
-    
-      handleChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
-      };
-    
-      componentDidMount() {
-        this.loadNotes();
-      }
-    
-      loadNotes = () => {
-        API.getNotes()
-          .then((res) => this.setState({ notes: res.data }))
-          .catch((err) => console.log(err));
-      };
-    
-      render() {
-          return (
-              <div>
-                  <div classname="container center">
-                  <Form  loadNotes={() => this.loadNotes()}/>
-                  <div className="row">
-                  <NoteCard
-            notes={this.state.notes}
-            loadNotes={() => this.loadNotes()}
-          />
-
-                  </div>
-                  </div>
-              </div>
-          )
->>>>>>> c37f2be6141ce989f4f1a556c415e20d43de71f5
 
   handleChange = (event) => {
     const { name, value } = event.target;
@@ -64,7 +26,7 @@ class Notes extends Component {
   render() {
     return (
       <div>
-        <div classname="container">
+        <div classname="container center">
           <Form loadNotes={() => this.loadNotes()} />
           <div className="row">
             <NoteCard
@@ -75,7 +37,6 @@ class Notes extends Component {
         </div>
       </div>
     )
-
   }
 }
 
