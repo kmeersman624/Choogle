6186096402bf4e38b6d69b81df4f4614a9e77e65
Tucker <tuckerbeauchamp@gmail.com>
2020-07-21 13:28:48 -0700

    fix private route rendering issue

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useHistory } from "react-router-dom";
 import { useUserContext } from "./UserContext";
 
 function PrivateRoute({ component: Component, ...rest }) {
   const { user } = useUserContext();
-
-  return (
-    // Show the component only when the user is logged in
-    // Otherwise, redirect the user to /login page
-    <Route
-      {...rest}
-      render={(props) =>
-        user.email ? <Component {...props} /> : <Redirect to="/login" />
-      }
-    />
-  );
+  // Show the component only when the user is logged in
+  // Otherwise, redirect the user to /login page
+  if (user.email) {
+    return <Route {...rest} render={(props) => <Component {...props} />} />;
+  } else {
+    return <Redirect to="/login" />;
+  }
 }
 
 export default PrivateRoute;
