840f57b5c553f19cc22dba8f7f547758425889e3
Marcus <mpt789@gmail.com>
2020-08-17 22:29:26 -0700

    added styles to search bar

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import "./styles.css";
+// import searchicon from '../../images/searchicon.png'
 
 export default function SearchBar() {
     return (
         <>
-                <div className="searchBorder">
-                    <form action="https://www.google.com/search" method="GET" className="row inline">
-                        <div className="inline">
-                            <input type="text" name="q" placeholder="Google Search" />
-                        </div>
-                        <div className="inline">
-                            <input type="submit" value="Google Search" />
-                        </div>
-                    </form>
+            <form action="https://www.google.com/search" method="GET" className="row inline searchbox">
+                <div className="inline td">
+                    <input type="text" name="q" placeholder="Google Search" size="25" className="search" />
                 </div>
+
+                <div class="inline">
+                    <button type="submit" className="waves-effect waves-light btn submit2">Search</button>
+                </div>
+            </form>
         </>
     )
 }
